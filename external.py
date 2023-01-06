7e6cd541589e4fc54bcf0114b1f17e6c48d26f5b
NUROISEA <120075289+NUROISEA@users.noreply.github.com>
2023-01-06 13:38:16 +0800

    different implementation

--- external.py
+++ external.py
@@ -1,5 +1,6 @@
 import os
-from subprocess import Popen, PIPE, CalledProcessError
+import subprocess
+import shlex
 
 hello_world = "Hello world, from an external python script!"
 
@@ -11,20 +12,24 @@ def fetch_repo_readme():
     #!wget https://github.com/NUROISEA/dumb-colab-tests/raw/main/README.md -O README.md
     os.system('wget https://github.com/NUROISEA/dumb-colab-tests/raw/main/README.md -O README.md')
 
-def continous_shell(cmd):
-    with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
-        for line in p.stdout:
-            print(line, end='') # process line here
-
-    if p.returncode != 0:
-        raise CalledProcessError(p.returncode, p.args)
+# https://www.endpointdev.com/blog/2015/01/getting-realtime-output-using-python/
+def run_command(command):
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print output.strip()
+    rc = process.poll()
+    return rc
 
 def clone_a_repo():
     cmd = 'git clone https://github.com/NUROISEA/stable-diffusion-webui.git'
-    continous_shell(cmd)
+    run_command(cmd)
     print("Done cloning")
 
 def download_large_file():
     cmd = 'wget https://huggingface.co/Linaqruf/anything-v3.0/resolve/main/Anything-V3.0.ckpt'
-    continous_shell(cmd)
+    run_command(cmd)
     print("Done downloading")
