e0bfb32ccfd8281140c42df0a05e5d5e2d6ede5b
NUROISEA <120075289+NUROISEA@users.noreply.github.com>
2023-01-06 12:48:00 +0800

    bro forgor python syntax

--- external.py
+++ external.py
@@ -2,9 +2,9 @@ hello_world = "Hello world, from an external python script!"
 
 
 
-def fetch_repo_license:
+def fetch_repo_license():
     !wget https://github.com/NUROISEA/dumb-colab-tests/raw/main/LICENSE -O LICENSE
 
-def fetch_repo_readme:
+def fetch_repo_readme():
     !wget https://github.com/NUROISEA/dumb-colab-tests/raw/main/README.md -O README.md
 
